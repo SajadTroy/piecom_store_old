32589ea827f1246ce865cbaf5c45d26705695d6b
mrsajadpp <mrsajadpp@gmail.com>
2025-04-05 12:43:15 +0530

    Implement user session handling and render index page

--- routes/index.js
+++ routes/index.js
@@ -7,5 +7,19 @@ const { send } = require('../email');
 
 const { notAuthorized, isAuthorized } = require('../middleware/auth');
 
+router.get('/', async (req, res) => {
+    try {
+        let user = req.session.user || null;
+        if (user) {
+            user =  await User.findById(user.id).select('-password');
+        }
+        console.log('User:', user);
+        
+        res.render('user/index', { title: 'Home', user });
+    } catch (error) {
+        console.error('Error rendering index page:', error.message);
+        res.status(500).send('An error occurred while rendering the page. Please try again.');
+    }
+});
 
 module.exports = router;
