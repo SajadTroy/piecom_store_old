f9f44e7bc3fdecd645a9a198229ce28d73abe5cd
mrsajadpp <mrsajadpp@gmail.com>
2025-04-06 10:22:59 +0530

    Remove outdated admin and user views for orders, products, and cart functionality, including associated scripts and styles.

--- models/Order.js
+++ models/Order.js
@@ -6,15 +6,79 @@ const orderSchema = new mongoose.Schema({
         ref: 'User',
         required: true
     },
-    address: {
-        addressLine1: String,
-        addressLine2: String,
-        landmark: String,
-        street: String,
-        city: String,
-        state: String,
-        zip: String,
-        country: String
+    shippingAddress: {
+        street: {
+            type: String,
+            default: '',
+        },
+        city: {
+            type: String,
+            default: '',
+        },
+        state: {
+            type: String,
+            enum: [
+                'Andaman and Nicobar Islands',
+                'Andhra Pradesh',
+                'Arunachal Pradesh',
+                'Assam',
+                'Bihar',
+                'Chandigarh',
+                'Chhattisgarh',
+                'Dadra and Nagar Haveli and Daman and Diu',
+                'Delhi',
+                'Goa',
+                'Gujarat',
+                'Haryana',
+                'Himachal Pradesh',
+                'Jammu and Kashmir',
+                'Jharkhand',
+                'Karnataka',
+                'Kerala',
+                'Ladakh',
+                'Lakshadweep',
+                'Madhya Pradesh',
+                'Maharashtra',
+                'Manipur',
+                'Meghalaya',
+                'Mizoram',
+                'Nagaland',
+                'Odisha',
+                'Puducherry',
+                'Punjab',
+                'Rajasthan',
+                'Sikkim',
+                'Tamil Nadu',
+                'Telangana',
+                'Tripura',
+                'Uttar Pradesh',
+                'Uttarakhand',
+                'West Bengal',
+                ''
+            ],
+            default: '',
+        },
+        zip: {
+            type: String,
+            default: '',
+        },
+        country: {
+            type: String,
+            default: 'India',
+            enum: ['India'],
+        },
+        addressLine1: {
+            type: String,
+            default: '',
+        },
+        addressLine2: {
+            type: String,
+            default: '',
+        },
+        landmark: {
+            type: String,
+            default: '',
+        },
     },
     products: [{
         productId: {
